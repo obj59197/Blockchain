945b37faeb1bfdc33a3fbac9f9f4554ef3ba205d
obj59197 <obj59197@uga.edu>
2018-03-11 20:02:20 -0400

    Create Block.h

---
+++ Block.h
@@ -0,0 +1,22 @@
+#include <cstdint>
+#include <iostream>
+
+using namespace std;
+
+class Block{
+  
+  public:
+    string strPrevious;
+    Block(uint32_t indexIn, const string &dataIn);
+    string getHash();
+    void mineBlock(uint32_t diff);
+  
+  private:
+    uint32_t index;
+    int64_t nonce;
+    string data;
+    string hash;
+    time_t time;
+
+    string calculateHash() const;
+}
